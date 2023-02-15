bf9c9afa4fe7e715ffb316f9eb87b5b6f3cff9cb
lingarajdp12 <lingarajachcholi1999@gmail.com>
2023-02-15 12:18:24 +0530

    push

---
+++ Student.java
@@ -0,0 +1,17 @@
+class Studnet 
+{
+static String std_name ="rajssh";
+static int std_fees=36000;
+static char std_sec='b';
+static String clg _name ="jspider";
+
+public static void main(String[] args)
+{
+System.out.println("student name is "+std_name);
+System.out.println("Student fees is "+std_fees);
+System.out.println("Student section is +"std_sec);
+System.out.println("The name college is "+clg_name);
+
+}
+
+}
